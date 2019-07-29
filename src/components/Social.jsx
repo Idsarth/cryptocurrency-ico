a755c10a671a5991cdf128c596bf70e8f28fc5cc
Idsarth <idsarthdev19@gmail.com>
2019-07-29 14:25:58 -0600

    redes sociales agregadas

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -4,9 +4,9 @@ import { IoLogoFacebook, IoLogoInstagram, IoLogoTwitter } from 'react-icons/io'
 const Social = () => {
   return (
     <div className='footer-social'>
-      <a href='' target='_black' ><IoLogoFacebook size='25' className='footer-icon' /></a>
-      <a href='' target='_black' ><IoLogoInstagram size='25' className='footer-icon' /></a>
-      <a href='' target='_black' ><IoLogoTwitter size='25' className='footer-icon' /></a>
+      <a href='' target='_blank' rel='noopener noreferrer' ><IoLogoFacebook size='22' className='footer-icon' /></a>
+      <a href='https://www.instagram.com/?hl=es-la' target='_blank' rel='noopener noreferrer' ><IoLogoInstagram size='22' className='footer-icon' /></a>
+      <a href='https://twitter.com/AlySystem' target='_blank' rel='noopener noreferrer' ><IoLogoTwitter size='22' className='footer-icon' /></a>
     </div>
   )
 }
