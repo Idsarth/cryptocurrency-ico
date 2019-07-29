a4aa7347f0bc925c33d7b42c920fa29cc6f09360
Idsarth <idsarthdev19@gmail.com>
2019-07-29 10:08:26 -0600

    error del eslint solucionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { Suspense } from 'react'
 import { Router } from '@reach/router'
 import Navbar from './components/Navbar'
-
-const Home = React.lazy(() => import('./pages/Home'))
-const About = React.lazy(() => import('./pages/About')) 
 import PageLoading from './pages/PageLoading'
+
+// const Home = React.lazy(() => import('./pages/Home'))
+import Home from './pages/Home'
+const About = React.lazy(() => import('./pages/About')) 
 
 const App = () => {
   return (
