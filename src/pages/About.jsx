5d51457df9df3dbb2e7ad300e737ff3ebb548834
Idsarth <idsarthdev19@gmail.com>
2019-07-29 18:08:36 -0600

    componentes terminados

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
     <Layout title='Nosotros' description=''>
       <Particles />
       <div className='global-max-content about'>
-        <h2 className='global-title-about'>Todo sobre nosotros</h2>
+        <h2 className='global-title-about' />
         <div className='about-content'>
           <p className='about-paragraph'>AlySystem se dedica al desarrollo de aplicaciones móviles bajo la tecnología BlockChain para brindarle a los usuarios mayor transparencia y seguridad a la hora de utilizarlas ya que esta tecnología permite guardar registros, transacciones, clave, documentos, contratos inteligentes mas seguro gracias a la cadena de bloques que almacena la información de forma encriptada.</p>
         </div>
